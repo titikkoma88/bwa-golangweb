a146adc6f5548f485bf38afd1920618ed45b06f1
titikkoma88 <jie.koma88@gmail.com>
2022-02-24 23:20:10 +0700

    render html

--- handler/handler.go
+++ handler/handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path"
 	"strconv"
 )
 
@@ -13,7 +15,24 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	w.Write([]byte("Welcome to Home"))
+
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
+	if err != nil {
+		log.Println(err)
+		//http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		//http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		
+		return
+	}
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
