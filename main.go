9a77dec77089c9b251c8631625a8667377506a6c
titikkoma88 <jie.koma88@gmail.com>
2022-02-24 21:46:57 +0700

    query string

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	aboutHandler := func (w http.ResponseWriter, r *http.Request)  {
-		w.Write([]byte("About Page"))
-	}
-
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Profile Page"))
-	})
+	mux.HandleFunc("/product", productHandler)
 
 	log.Println("Starting web on port 8080")
 
@@ -37,4 +32,16 @@ func homeHandler(w http.ResponseWriter, r *http.Request)  {
 
 func helloHandler(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte("Hello world, saya sedang belajar Golng Web"))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request)  {
+	id := r.URL.Query().Get("id")
+	idNumb, err := strconv.Atoi(id)
+	
+	if err != nil || idNumb < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Product Page : %d", idNumb)
 }
